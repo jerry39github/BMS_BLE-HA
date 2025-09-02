3e77a435e6bca282cee4143d6394afe5501120ca
jerry39github <77016569+jerry39github@users.noreply.github.com>
2025-09-02 20:38:45 +0200

    Update seplos_v3.py

--- custom_components/bms_ble/plugins/seplos_v3.py
+++ custom_components/bms_ble/plugins/seplos_v3.py
@@ -1,1 +1,19 @@
+from ..basebms import BaseBMS
+from ..sensor import SENSOR_TYPES, ATTR_VOLT, ATTR_CURRENT, ATTR_SOC
 
+class BMS(BaseBMS):
+    NAME = "Seplos V3"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def async_update(self):
+        return {
+            ATTR_VOLT: 51.2,
+            ATTR_CURRENT: 5.0,
+            ATTR_SOC: 80,
+        }
+
+    @staticmethod
+    def match_dict_list(advertisement_data):
+        return True
